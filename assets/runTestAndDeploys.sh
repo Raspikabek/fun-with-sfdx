cb6932c1f610d5fd9b4298631a0b13af2b0b0da0
Raspikabek <asenjo.rodriguez.eduardo@gmail.com>
2019-02-16 02:12:09 +0000

    testing new ways...

--- assets/runTestAndDeploys.sh
+++ assets/runTestAndDeploys.sh
@@ -14,7 +14,7 @@ if [ "$OPERATION" = "TestOnly" ]; then
     echo "Running Validation against" $ENVIRONMENT
     OUTPUT="$(sfdx force:mdapi:deploy -c -l RunLocalTests -d temp_metadata/ -u DevHub -w 10)"
     echo "${OUTPUT}"
-    if [ "$OUTPUT" == *"ERROR"* ]; then
+    if [ $OUTPUT == *_"ERROR"_* ]; then
         echo "Im IN!"
         exit 1
     fi
